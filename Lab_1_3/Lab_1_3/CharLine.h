b0c90eb78341f78579efef32c745d58fb9bc71ba
Laura Shcherbak <laura.shcherbak@gmail.com>
2021-02-28 22:08:14 +0200

    Add lab 1.3

---
+++ Lab_1_3/Lab_1_3/CharLine.h
@@ -0,0 +1,31 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+// CharLine.h
+
+#pragma once
+#include <string>
+
+using namespace std;
+
+class CharLine
+{
+private:
+	int N;
+	char *s;
+
+public:
+	bool setN(int value);
+	void setS(char* value);
+	int getN() const { return N; };
+	char* getS() const { return s; };
+
+	bool Init(int N);
+	void Read(int N);
+	void Display();
+	void DisplaySn(int i);
+	void CheckChar(char ch);
+	void Сoncatenation(char* s1, char* s2);
+	void CompareStr(CharLine s1, CharLine s2);
+	void CheckSubStr(string search);
+	string toString();
+
+};
---
+++ Lab_1_3/Lab_1_3/Source.cpp
@@ -0,0 +1,36 @@
+﻿//////////////////////////////////////////////////////////////////////////////
+// Source.cpp
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif // !#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include "CharLine.h"
+
+int main()
+{
+	CharLine c;
+	int N;
+	cout << "Enter N : ";
+	cin >> N;
+	c.Read(N);
+	c.Display();
+	cout << "toString() = " << c.toString();
+
+	c.DisplaySn(2);
+
+	char ch;
+	cout << "Enter symbol for test : ";
+	cin >> ch;
+	c.CheckChar(ch);
+
+	CharLine c2;
+	c2.Init(5);
+	c2.Read(5);
+	c.Сoncatenation(c.getS(), c2.getS());
+
+	c.CompareStr(c, c2);
+	c.CheckSubStr(c2.getS());
+
+	return 0;
+}
